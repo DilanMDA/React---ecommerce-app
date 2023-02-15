b828c37ecb007dc462869692a2a52e12da226ca3
DilanMDA <akiladilan04@gmail.com>
2023-02-15 09:32:26 +0530

    marquee section implement

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -55,16 +55,16 @@ const Footer = () => {
                   akiladilan04@gmail.com
                 </a>
                 <div className='social-icons d-flex align-items-center gap-30 mt-4'>
-                  <a href='' className='text-white'>
+                  <a href='#' className='text-white'>
                     <BsLinkedin className='fs-4' />
                   </a>
-                  <a href='' className='text-white'>
+                  <a href='#' className='text-white'>
                     <BsInstagram className='fs-4' />
                   </a>
-                  <a href='' className='text-white'>
+                  <a href='#' className='text-white'>
                     <BsGithub className='fs-4' />
                   </a>
-                  <a href='' className='text-white'>
+                  <a href='#' className='text-white'>
                     <BsYoutube className='fs-4' />
                   </a>
                 </div>
