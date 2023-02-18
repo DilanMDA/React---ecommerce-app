c9ec4ee01327b130b88226662995ed4f2918e013
DilanMDA <akiladilan04@gmail.com>
2023-02-18 10:41:01 +0530

    popular product initial state created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -227,6 +227,21 @@ const Home = () => {
           </div>
         </div>
       </section>
+      <section className='popular-wrapper home-wrapper py-5'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-12'>
+              <h3 className='section-heading'> Our Popular Products </h3>
+            </div>
+          </div>
+          <div className='row'>
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+          </div>
+        </div>
+      </section>
       <section className='marque-wrapper home-wrapper-2 py-5'>
         <div className='container-xxl'>
           <div className='row'>
