d373df9ed152b4a330e6d00e724b03cc12d6ea15
DilanMDA <akiladilan04@gmail.com>
2023-02-19 11:59:36 +0530

    Our-store initiate

---
+++ src/pages/OutStore.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Meta from "../components/Meta";
+import BreadCrumb from "./../components/BreadCrumb";
+
+const OutStore = () => {
+  return (
+    <>
+      <Meta title='Our Store' />
+      <BreadCrumb title='Our Store' />
+    </>
+  );
+};
+
+export default OutStore;
