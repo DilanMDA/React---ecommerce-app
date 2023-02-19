bfc6a5a32cd8d1ecb3aa373b17e8a7ba159d51e6
DilanMDA <akiladilan04@gmail.com>
2023-02-20 03:51:43 +0530

    Shop By & Filter By feature implemented

--- src/pages/OutStore.js
+++ src/pages/OutStore.js
@@ -7,6 +7,125 @@ const OutStore = () => {
     <>
       <Meta title='Our Store' />
       <BreadCrumb title='Our Store' />
+      <div className='store-wrapper py-5 home-wrapper-2'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-3'>
+              <div className='filter-card mb-3'>
+                <h3 className='filter-title'>Shop By Category</h3>
+                <div>
+                  <ul>
+                    <li>Watch</li>
+                    <li>TV</li>
+                    <li>Camera</li>
+                    <li>Laptop</li>
+                  </ul>
+                </div>
+              </div>
+              <div className='filter-card mb-3'>
+                <h3 className='filter-title'>Filter By</h3>
+                <div>
+                  <h5 className='sub-title'>Availability</h5>
+                  <div>
+                    <div className='form-check'>
+                      <input
+                        className='form-check-input'
+                        type='checkbox'
+                        value=''
+                        id=''
+                      />
+                      <label className='form-check-label' htmlFor=''>
+                        In Stock (1)
+                      </label>
+                    </div>
+                    <div className='form-check'>
+                      <input
+                        className='form-check-input'
+                        type='checkbox'
+                        value=''
+                        id=''
+                      />
+                      <label className='form-check-label' htmlFor=''>
+                        Out of stock (0)
+                      </label>
+                    </div>
+                  </div>
+                  <h5 className='sub-title'>Price</h5>
+                  <div className='d-flex align-content-center gap-10'>
+                    <div className='form-floating'>
+                      <input
+                        type='email'
+                        className='form-control'
+                        id='floatingInput'
+                        placeholder='From'
+                      />
+                      <label htmlFor='floatingInput'>From</label>
+                    </div>
+                    <div className='form-floating'>
+                      <input
+                        type='email'
+                        className='form-control'
+                        id='floatingInput1'
+                        placeholder='To'
+                      />
+                      <label htmlFor='floatingInput1'>To</label>
+                    </div>
+                  </div>
+                  <h5 className='sub-title'>Colors</h5>
+                  <div>
+                    <ul className='colors'>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                    </ul>
+                  </div>
+                  <h5 className='sub-title'>Size</h5>
+                  <div>
+                    <div className='form-check'>
+                      <input
+                        className='form-check-input'
+                        type='checkbox'
+                        value=''
+                        id='color-1'
+                      />
+                      <label className='form-check-label' htmlFor='color-1'>
+                        S (2)
+                      </label>
+                    </div>
+                    <div className='form-check'>
+                      <input
+                        className='form-check-input'
+                        type='checkbox'
+                        value=''
+                        id='color-2'
+                      />
+                      <label className='form-check-label' htmlFor='color-2'>
+                        m (2)
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='filter-card mb-3'>
+                <h3 className='filter-title'>Product Tags</h3>
+              </div>
+              <div className='filter-card mb-3'>
+                <h3 className='filter-title'>Random product</h3>
+              </div>
+            </div>
+            <div className='col-9'></div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
