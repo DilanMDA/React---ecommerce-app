d685a222325e172503ba7111d1de31d5802a7187
DilanMDA <akiladilan04@gmail.com>
2023-02-21 04:00:38 +0530

    filter grid completed

--- src/pages/OutStore.js
+++ src/pages/OutStore.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactStars from "react-rating-stars-component";
 import Meta from "../components/Meta";
 import BreadCrumb from "./../components/BreadCrumb";
 
@@ -117,12 +118,134 @@ const OutStore = () => {
               </div>
               <div className='filter-card mb-3'>
                 <h3 className='filter-title'>Product Tags</h3>
+                <div>
+                  <div className='product-tags d-flex flex-wrap align-item-center gap-10'>
+                    <span className='badge bg-light text-secondary rounded-3 py-2 px-3'>
+                      Headphone
+                    </span>
+                    <span className='badge bg-light text-secondary rounded-3 py-2 px-3'>
+                      Laptop
+                    </span>
+                    <span className='badge bg-light text-secondary rounded-3 py-2 px-3'>
+                      Mobile
+                    </span>
+                    <span className='badge bg-light text-secondary rounded-3 py-2 px-3'>
+                      Wire
+                    </span>
+                  </div>
+                </div>
               </div>
               <div className='filter-card mb-3'>
                 <h3 className='filter-title'>Random product</h3>
+                <div>
+                  <div className='random-products mb-3 d-flex '>
+                    <div className='w-50'>
+                      <img
+                        src='images/watch.jpg'
+                        className='img-fluid'
+                        alt='watch'
+                      />
+                    </div>
+                    <div className='w-50'>
+                      <h5>
+                        Kids headphones bulk 10 pack multi colored for students
+                      </h5>
+                      <ReactStars
+                        count={5}
+                        // onChange={ratingChanged}
+                        value={4}
+                        edit={false}
+                        size={24}
+                        activeColor='#ffd700'
+                      />
+                      <b>$ 300</b>
+                    </div>
+                  </div>
+                  <div className='random-products mb-3 d-flex '>
+                    <div className='w-50'>
+                      <img
+                        src='images/watch.jpg'
+                        className='img-fluid'
+                        alt='watch'
+                      />
+                    </div>
+                    <div className='w-50'>
+                      <h5>
+                        Kids headphones bulk 10 pack multi colored for students
+                      </h5>
+                      <ReactStars
+                        count={5}
+                        // onChange={ratingChanged}
+                        value={4}
+                        edit={false}
+                        size={24}
+                        activeColor='#ffd700'
+                      />
+                      <b>$ 300</b>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className='col-9'></div>
+            <div className='col-9'>
+              <div className='filter-sort-grid mb-4'>
+                <div className='d-flex justify-content-between align-items-center'>
+                  <div className='d-flex align-items-center gap-10'>
+                    <p className='mb-0 d-block ' style={{ width: "100%" }}>
+                      Sort By:
+                    </p>
+                    <select name='' id='' className='form-control form-select'>
+                      <option value='manual'>Featured</option>
+                      <option value='best-selling' selected='selected'>
+                        Best Selling
+                      </option>
+                      <option value='title-ascending'>
+                        Alphabetically, A-Z
+                      </option>
+                      <option value='title-descending'>
+                        Alphabetically, Z-A
+                      </option>
+                      <option value='Price-ascending'>Price,low to high</option>
+                      <option value='price-descending'>
+                        Price,high to low
+                      </option>
+                      <option value='created-ascending'>
+                        Date, old to new
+                      </option>
+                      <option value='created-descending'>
+                        Date, new to old
+                      </option>
+                    </select>
+                  </div>
+                  <div className='d-flex align-items-center gap-10'>
+                    <p className='total-products mb-0'>21 Products</p>
+                    <div className='d-flex gap-10 align-items-center grid'>
+                      <img
+                        src='images/gr4.svg'
+                        className='d-block img-fluid'
+                        alt='grid'
+                      />
+                      <img
+                        src='images/gr3.svg'
+                        className='d-block img-fluid'
+                        alt='grid'
+                      />
+                      <img
+                        src='images/gr2.svg'
+                        className='d-block img-fluid'
+                        alt='grid'
+                      />
+
+                      <img
+                        src='images/gr.svg'
+                        className='d-block img-fluid'
+                        alt='grid'
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
