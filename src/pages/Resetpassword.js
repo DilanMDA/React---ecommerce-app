5de5cf08bd9b536c1219a454763cfcdef3536273
DilanMDA <akiladilan04@gmail.com>
2023-02-24 03:03:29 +0530

    signup page and Reset Password page implemented

--- src/pages/Resetpassword.js
+++ src/pages/Resetpassword.js
@@ -2,43 +2,19 @@ import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 
-const Signup = () => {
+const Resetpassword = () => {
   return (
     <>
-      <Meta title={"Sign Up"} />
-      <BreadCrumb title='Sign Up' />
+      <Meta title={"Reset Password"} />
+      <BreadCrumb title='Reset Password' />
       <div className='login-wrapper home-wrapper-2 py-5'>
         <div className='container-xxl'>
           <div className='row'>
             <div className='col-12'>
               <div className='auth-card'>
-                <h3 className='text-center mb-3'>Create Account</h3>
+                <h3 className='text-center mb-3'> Reset Password</h3>
                 <form action='' className='d-flex flex-column gap-15'>
                   <div>
-                    <input
-                      type='text'
-                      name='name'
-                      placeholder='Name'
-                      className='form-control'
-                    />
-                  </div>
-                  <div>
-                    <input
-                      type='email'
-                      name='email'
-                      placeholder='Email'
-                      className='form-control'
-                    />
-                  </div>
-                  <div>
-                    <input
-                      type='tel'
-                      name='mobile'
-                      placeholder='Mobile Number'
-                      className='form-control'
-                    />
-                  </div>
-                  <div className='mt-1'>
                     <input
                       type='password'
                       name='password'
@@ -46,9 +22,17 @@ const Signup = () => {
                       className='form-control'
                     />
                   </div>
+                  <div className='mt-1'>
+                    <input
+                      type='password'
+                      name='confpassword'
+                      placeholder='Confirm Password'
+                      className='form-control'
+                    />
+                  </div>
                   <div>
                     <div className='mt-3 d-flex justify-content-center gap-15 align-items-center'>
-                      <button className='button border-0'> Sign Up </button>
+                      <button className='button border-0'> Ok </button>
                     </div>
                   </div>
                 </form>
@@ -61,4 +45,4 @@ const Signup = () => {
   );
 };
 
-export default Signup;
+export default Resetpassword;
